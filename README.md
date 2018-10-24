c670f4eeb9afbd2011ef905c89ae1f68c31ced9a
Unknown <nattapat27@gmail.com>
2018-10-24 19:26:57 +0700

    [FIX] deploy webpack

--- README.md
+++
@@ -1,4 +0,0 @@
-modify from https://github.com/nkholski/phaser3-es6-webpack
-Set up
-1.yarn
-2.yarn dev
--- src/socket.js
+++
@@ -1,3 +0,0 @@
-import socketClient from 'socket.io-client'
-const socket = socketClient.connect('http://localhost:3002');
-export default socket;
