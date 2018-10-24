87167e4400508aeed81755239c7a887bde0101b4
Unknown <nattapat27@gmail.com>
2018-10-24 19:27:30 +0700

    [FIX] forget file change

---
+++ src/main.js
@@ -0,0 +1,23 @@
+import 'phaser';
+import GameScene from './scenes/GameScene';
+
+const config = {
+    // For more settings see <https://github.com/photonstorm/phaser/blob/master/src/boot/Config.js>
+    type: Phaser.WEBGL,
+    pixelArt: true,
+    roundPixels: true,
+    parent: 'content',
+    width: 400,
+    height: 240,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            debug: false
+        }
+    },
+    scene: [
+        GameScene
+    ]
+};
+
+const game = new Phaser.Game(config);
---
+++ src/scenes/GameScene.js
@@ -0,0 +1,21 @@
+class GameScene extends Phaser.Scene {
+    constructor(test) {
+        super({
+            key: 'GameScene'
+        });
+    }
+
+    preload() {
+        
+    }
+
+    create() {
+        
+    }
+
+    update() {
+
+    }
+}
+
+export default GameScene;
